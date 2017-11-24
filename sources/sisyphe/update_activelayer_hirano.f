6e9ed9ebdfb1a2d88421e544d8c88b81205846fc
Sara PAVAN <G87073@dsp0677866.postes.calibre.edf.fr>
2017-11-24 10:33:25 +0100

    [src] Initialization and mean grain size
    - computation of masses in init sediment, starting from mass ratio
    - mean grain size computed with mass ratio, if new bed model

--- sources/sisyphe/update_activelayer_hirano.f
+++ sources/sisyphe/update_activelayer_hirano.f
@@ -1,7 +1,6 @@
-
-!                    *****************
+!            ************************************
              SUBROUTINE UPDATE_ACTIVELAYER_HIRANO
-!                    *****************
+!            ************************************
 !
 !***********************************************************************
 ! SISYPHE   V7P3
@@ -9,8 +8,8 @@
 !
 !     brief    MASS TRANSFER BETWEEN THE ACTIVE LAYER (FIRST LAYER) AND THE
 !+             LAYER UNDERNEATH. THIS IS POSSIBLE AND NEEDED ONLY IF THERE
-!+             IS NO CONSOLIDATION. 
-!+             FOR THE CASE OF CONSOLIDATION, THE MASS TRANSFER BETWEEN LAYERS   
+!+             IS NO CONSOLIDATION.
+!+             FOR THE CASE OF CONSOLIDATION, THE MASS TRANSFER BETWEEN LAYERS
 !+             IS ONLY CAUSED BY CONSOLIDATION AND THE ACTIVE LAYER IS VIRTUAL
 !+             AND THEREFORE RECOMPUTED WHEN NEEDED FROM THE CONSOLIDATION LAYERS
 !+
@@ -20,7 +19,7 @@
 !
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !| XXX       |-->| XXX
-!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~             
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       USE BIEF
       USE DECLARATIONS_TELEMAC
       USE DECLARATIONS_SISYPHE
@@ -28,7 +27,7 @@
 
 !
       IMPLICIT NONE
-      
+
       INTEGER LNG,LU
       COMMON/INFO/LNG,LU
 
@@ -42,7 +41,7 @@
 !
       IF(NLAYER.LT.2) THEN ! in this case no need to compute transfer of mass between layers, so no need to pass through this routine
          WRITE(LU,*) 'ONLY ONE LAYER : NO ACTIVE LAYER MODEL POSSIBLE'
-         CALL PLANTE(1) 
+         CALL PLANTE(1)
          STOP
       ENDIF
 
@@ -107,33 +106,33 @@
           ES(IPOIN,1) = MASS_SAND_TOT(1,IPOIN)/XMVS
      &                  +ES_MUD_ONLY(IPOIN,1)
         ELSE
-	     ES(IPOIN,1) = MASS_SAND_TOT(1,IPOIN)/((1-XKV(1))*XMVS)
+          ES(IPOIN,1) = MASS_SAND_TOT(1,IPOIN)/((1-XKV(1))*XMVS)
       ENDIF
-!      
-      THICK_TRANSFER=ESTRAT%R(IPOIN)-ES(IPOIN,1) ! negative sign means from layer 1 to layer 2 
+!
+      THICK_TRANSFER=ESTRAT%R(IPOIN)-ES(IPOIN,1) ! negative sign means from layer 1 to layer 2
 !-----------------------------------------------------------------------
 !      DEPOSITION CASE
 !-----------------------------------------------------------------------
-!          
+!
         IF(THICK_TRANSFER.GT.0.D0) THEN ! active layer too large: transfer of mass needed to layer 2
 
           IF(NMUD.GE.1) THEN
             DO IMUD = 1,NMUD
               FLUX_MASS_MUD(IMUD,IPOIN)=(THICK_TRANSFER/ES(IPOIN,1))
-     &       *MASS_MUD_TOT(1,IPOIN)*RATIO_MUD(IMUD,1,IPOIN)  
+     &       *MASS_MUD_TOT(1,IPOIN)*RATIO_MUD(IMUD,1,IPOIN)
             ENDDO
           ENDIF
           IF(NSAND.GE.1) THEN
             DO ISAND=1,NSAND
               FLUX_MASS_SAND(ISAND,IPOIN)=(THICK_TRANSFER/ES(IPOIN,1))
-     &       *MASS_SAND_TOT(1,IPOIN) *RATIO_SAND(ISAND,1,IPOIN)  
+     &       *MASS_SAND_TOT(1,IPOIN) *RATIO_SAND(ISAND,1,IPOIN)
             ENDDO
           ENDIF
-!      
+!
 !-----------------------------------------------------------------------
 !      EROSION CASE
 !-----------------------------------------------------------------------
-!    
+!
           ELSEIF(THICK_TRANSFER.LT.0.D0) ! active layer too small: transfer of mass needed from layer 2
              THICK_TRANSFER=MIN(THICK_TRANSFER,ES(IPOIN,2)) ! if not enough sediment in layer 2 active layer thickness will be smaller than ESTRAT
              IF(MASS_SAND_TOT(2,IPOIN).GT.1.D-9) THEN
@@ -157,13 +156,13 @@
                DO ISAND=1,NSAND
                   FLUX_MASS_SAND(ISAND,IPOIN)=
      &            (THICK_TRANSFER/ES(IPOIN,2))
-     &            *MASS_SAND_TOT(2,IPOIN)*RATIO_SAND(ISAND,2,IPOIN)      
+     &            *MASS_SAND_TOT(2,IPOIN)*RATIO_SAND(ISAND,2,IPOIN)
                ENDDO
              ENDIF
-          ENDIF ! end test 2nd layer large enough        
-        ENDIF ! end test 1st layer large enough 
+          ENDIF ! end test 2nd layer large enough
+        ENDIF ! end test 1st layer large enough
       ENDDO
-!      
+!
 !-----------------------------------------------------------------------
 !     TRANSFER OF MUD MASSES
 !-----------------------------------------------------------------------
@@ -178,7 +177,7 @@
           ENDDO
         ENDDO
       ENDIF
-!      
+!
 !-----------------------------------------------------------------------
 !     TRANSFER OF SAND MASSES
 !-----------------------------------------------------------------------
