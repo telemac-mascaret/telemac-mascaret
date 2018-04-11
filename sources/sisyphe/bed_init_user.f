2c3b8424cf20f490c018e46a42194b5b9e3826ba
LIBLHF <LIBLHF@arteliagroup.com>
2018-04-11 17:59:45 +0200

    ajout de bed_init_user.f à envoi precedent

---
+++ sources/sisyphe/bed_init_user.f
@@ -0,0 +1,54 @@
+!                    ************************
+                     SUBROUTINE BED_INIT_USER
+!                    ************************
+!
+     &(ESTRATUM,RATIO_INIT)
+!
+!***********************************************************************
+! 
+      USE BIEF
+      USE DECLARATIONS_SPECIAL
+      USE DECLARATIONS_SISYPHE
+      IMPLICIT NONE
+!
+      DOUBLE PRECISION,INTENT(INOUT):: ESTRATUM(NPOIN,NOMBSTRAT)
+      DOUBLE PRECISION,INTENT(INOUT):: 
+     &     RATIO_INIT(NSICLA,NOMBSTRAT,NPOIN)
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      INTEGER            :: IPOIN,ICLA,ISTRAT
+!	  
+!======================================================================!
+!======================================================================!
+!                               PROGRAM                                !
+!======================================================================!
+!======================================================================!
+!
+! DEFAULT CASE : NO STRATIFICATION = ONLY ONE STRATUM
+
+      IF (NOMBSTRAT.EQ.1) THEN
+		DO IPOIN=1,NPOIN
+		    ESTRATUM(IPOIN,1)=100.D0 ! User can change the thickness of sediment here (replaces noerod.f)
+            DO ICLA=1,NSICLA
+              RATIO_INIT(ICLA,1,IPOIN) = AVA0(ICLA)
+			ENDDO
+		ENDDO
+      ELSE
+    	  
+!  USERS MUST DEFINE LAYER THICKNESS AND COMPOSITION FOR EACH STRATUM 
+		DO IPOIN=1,NPOIN
+		 DO ISTRAT=1,NOMBSTRAT
+		    ESTRATUM(IPOIN,ISTRAT)=0.D0 ! par défaut les couches sont vides
+            DO ICLA=1,NSICLA
+              RATIO_INIT(ICLA,ISTRAT,IPOIN) = 1.D0/NSICLA 
+			ENDDO			
+		 ENDDO
+         ! user must specify non-voids layers below	
+         !...		 
+		ENDDO
+      ENDIF
+!
+!-----------------------------------------------------------------------
+!
+      RETURN
+      END
